c3d862e20b80acccde3eddba0e6c78940198ab24
Nixson Benitez <92173600+nixsonbenitez@users.noreply.github.com>
2026-05-02 20:10:53 -0500

    Here is the schema  for the db

--- db/schema.sql
+++ db/schema.sql
@@ -1,1 +1,33 @@
--- TODO
+DROP TABLE IF EXISTS orders_products;
+DROP TABLE IF EXISTS orders;
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS products; 
+
+CREATE TABLE users(
+ id SERIAL PRIMARY KEY,
+ username text NOT NULL UNIQUE,
+ password text NOT NULL
+);
+
+CREATE TABLE orders (
+ id serial PRIMARY KEY,
+ date date NOT NULL,
+ note text,
+ user_id integer NOT NULL REFERENCES users(id) ON DELETE CASCADE
+);
+
+
+CREATE TABLE products(
+id serial PRIMARY KEY ,
+title text NOT NULL,
+description text NOT NULL,
+price decimal NOT NULL
+);
+
+CREATE TABLE orders_products(
+id serial PRIMARY KEY,
+quantity integer NOT NULL,
+product_id integer NOT NULL REFERENCES products(id) ON DELETE CASCADE,
+order_id integer NOT NULL REFERENCES orders(id) ON DELETE CASCADE,
+unique(product_id, order_id)
+);
